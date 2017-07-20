cf613bf7bbe7d9b100dacd66b38c8b0d77a8d84a
sahuj6 <sourabh.ahuja@aexp.com>
2017-07-19 23:11:48 -0700

    Lists

--- src/com/ds/trees/CountLeafNodesInTree.java
+++ src/com/ds/trees/CountLeafNodesInTree.java
@@ -1,8 +1,42 @@
 package com.ds.trees;
 
+import java.util.Arrays;
+
 /**
  * Created by Sourabh on 7/15/2017.
  * http://www.geeksforgeeks.org/write-a-c-program-to-get-count-of-leaf-nodes-in-a-binary-tree/
  */
 public class CountLeafNodesInTree {
+
+    public static int traverse(BinaryTree.Node node, int size) {
+        if(node == null) {
+            return 0;
+        }
+
+        if(node.left == null && node.right == null) {
+            return ++size;
+        }
+
+        size = traverse(node.left, size);
+        size = traverse(node.right, size);
+        return size;
+    }
+
+    static int getLeafCount(BinaryTree.Node node)
+    {
+        if (node == null)
+            return 0;
+        if (node.left == null && node.right == null)
+            return 1;
+        else
+            return getLeafCount(node.left) + getLeafCount(node.right);
+    }
+
+    public static void main(String[] args) {
+        BinaryTree bt = BinaryTree.makeTree();
+        int size = traverse(bt.root, 0);
+        System.out.println(size);
+
+        System.out.println(getLeafCount(bt.root));
+    }
 }
--- src/com/ds/trees/PrintAllRootToLeafPaths.java
+++ src/com/ds/trees/PrintAllRootToLeafPaths.java
@@ -1,8 +1,34 @@
 package com.ds.trees;
 
+import java.util.Arrays;
+import java.util.Stack;
+
 /**
  * Created by Sourabh on 7/15/2017.
  * http://www.geeksforgeeks.org/given-a-binary-tree-print-out-all-of-its-root-to-leaf-paths-one-per-line/
  */
+
 public class PrintAllRootToLeafPaths {
+    private static Stack<String> stack = new Stack<String>();
+
+    public static void traverse(BinaryTree.Node node) {
+        if(node == null) {
+            return;
+        }
+
+        stack.push(node.data);
+        if(node.left == null && node.right == null) {
+            System.out.println(Arrays.asList(stack.toArray()));
+            return;
+        }
+        traverse(node.left);
+        stack.pop();
+        traverse(node.right);
+        stack.pop();
+    }
+
+    public static void main(String[] args) {
+        BinaryTree bt = BinaryTree.makeTree();
+        traverse(bt.root);
+    }
 }
